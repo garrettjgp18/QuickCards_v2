49ab48624393fad429763140b3b71aa93344b14c
Joseph Penton <josephpenton2@gmail.com>
2024-02-24 20:38:13 -0600

    Added JSON data for # of cards and schema.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 
-/* 
-
-This is the hub of the server - all endpoint request will route through here. 
-
+/*
+  This file contains the main logic of NodeJS, including the routes and POST / GET fetch request processing. 
 */
 
 
@@ -22,20 +20,29 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+
+// ROUTES / ENDPOINTS --------------------------------------------
+
+
 // Handles POST request to the /api endpoint
 app.post('/api', (req, res) => {
   const recievedData = req.body;
   // Ensure button information was correct
-  console.log("Recieved: " + recievedData.mediaType);
+  console.log(`Recieved from React:\n Media Type: ${recievedData.mediaType}\n Number of Cards = ${recievedData.numberOfCards}\n Schema: ${recievedData.currentSchema}`);
+
 
   // Send message back to React page
-  res.json({message: "Data Recieved"});
+  res.json({message: "Data Recieved from NodeJS"});
 });
+
 
 // Handles GET request to the API endpoint
 app.get('/api', (req, res) => {
   res.send('This is the API endpoint');
 });
+
+
+// CREATE -------------------------------------------------------
 
 
 // Ensure server is running and endpoint is working
--- frontend/quickcards/src/pages/CreatePage.jsx
+++ frontend/quickcards/src/pages/CreatePage.jsx
@@ -1,112 +1,100 @@
-
 import React, { useState } from 'react';
-
 
 export default function Navbar(){
 
-    //define variable which takes the name of the mode and sets it to the variable 
+    // Define state variables (hooks)
     const [currentId, setCurrentId] = useState("Video");
+    const [numberOfCards, setNumberOfCards] = useState(""); // State variable for number of cards - setNumberOfCards will update value once changed
+    const [currentSchema, setCurrentSchema] = useState(""); 
 
-    const selectMode = (id) =>{
-        //set current id 
+    // Function to update number of cards state
+    const handleNumberOfCardsChange = (event) => {
+        setNumberOfCards(event.target.value);
+    }
+
+
+    const changeCurrentSchema = (event) => {
+        setCurrentSchema(event.target.value);
+    }
+
+    const selectMode = (id) => {
         setCurrentId(id);
     }
 
-    // Stays in frontend
-    const uploadContent = () =>{
+    const uploadContent = () => {
         console.log("Upload Content");
     }
 
-
-    /* 
-    Purpose: Send POST request to NodeJS server with user selected components
-    */
     const submitData = async () => {
-        // Assign JSON that holds data being sent - currentID is a useState in React (above)
         const dataObject = {
-            mediaType: `${currentId}`
+            mediaType: `${currentId}`,
+            numberOfCards: numberOfCards,
+            currentSchema: currentSchema
         };
-    
+
         try {
-            // Send a Fetch request to our full URL (including endpoint)
             const response = await fetch('http://127.0.0.1:3000/api', {
-                // Set method as Post for sending data
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                // Convert dataObject to JSON and set as request body
                 body: JSON.stringify(dataObject)
             });
-    
-            // Check if the response is okay
+
             if (!response.ok) {
-                // If not okay, throw an error
                 throw new Error("No network response");
             }
-    
-            // Await response from NodeJS server
+
             const responseData = await response.json();
-            // Log the response from the server
             console.log("Response from server: " + responseData.message);
         } catch(error) {
-            // Catch any errors that occur during the fetch operation
             console.log("Error sending data to server: " + error);
         }
-    
-        // Log a message indicating that the data submission process is complete
-        console.log("Submit Data");
+
+        console.log("Transfer complete");
     }
-    
 
     return (
         <>
-
-        {/*Container */}
-        <div className = "min-h-[70vh] h-auto w-3/4 border m-auto mt-12 rounded-xl p-4 shadow-lg">
-            {/*Top Box with Options */}
-            <div className = "w-4/5 h-16 flex items-center justify-center ml-auto mr-auto">
+        <div className="min-h-[70vh] h-auto w-3/4 border m-auto mt-12 rounded-xl p-4 shadow-lg">
+            <div className="w-4/5 h-16 flex items-center justify-center ml-auto mr-auto">
                 <ul className="w-auto mt-4 flex flex-row gap-12 md:gap-16 lg:gap-24 xl:gap-28 2xl:gap-36 justify-center align-middle text-2xl md:text-xl text-gray-600">
-                    {/* Hide text and show icons when in mobile view */}
-                    <li onClick={() => selectMode("Video")} className= {`cursor-pointer flex flex-row gap-2 ${currentId === "Video" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
+                    <li onClick={() => selectMode("Video")} className={`cursor-pointer flex flex-row gap-2 ${currentId === "Video" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
                         <span className="hidden md:block">Video</span>
                         <span className="text-3xl md:text-2xl relative bottom-0.5"><i className="bi-camera-video"></i></span>
                     </li>
-                    <li onClick={() => selectMode("PDF")} className= {`cursor-pointer flex flex-row gap-2 ${currentId === "PDF" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
+                    <li onClick={() => selectMode("PDF")} className={`cursor-pointer flex flex-row gap-2 ${currentId === "PDF" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
                         <span className="hidden md:block">PDF</span>
                         <span className="text-3xl md:text-2xl relative bottom-0.5"><i className="bi-file-pdf"></i></span>
                     </li>
-                    <li onClick={() => selectMode("Text")} className= {`cursor-pointer flex flex-row gap-2 ${currentId === "Text" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
+                    <li onClick={() => selectMode("Text")} className={`cursor-pointer flex flex-row gap-2 ${currentId === "Text" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
                         <span className="hidden md:block">Text</span>
                         <span className="text-3xl md:text-2xl relative bottom-0.5"><i className="bi-text-paragraph"></i></span>
                     </li>
-                    <li onClick={() => selectMode("Audio")} className= {`cursor-pointer flex flex-row gap-2 ${currentId === "Audio" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
+                    <li onClick={() => selectMode("Audio")} className={`cursor-pointer flex flex-row gap-2 ${currentId === "Audio" ? 'text-teal-500' : 'hover:text-teal-500 active:scale-95'}`}>
                         <span className="hidden md:block">Audio</span>
                         <span className="text-3xl md:text-2xl relative bottom-0.5"><i className="bi-headphones"></i></span>
                     </li>
                 </ul>
             </div>
 
-            <hr className = "bg-gray-200 h-0.5 w-full md:w-[90%] mr-auto ml-auto mt-1" />
+            <hr className="bg-gray-200 h-0.5 w-full md:w-[90%] mr-auto ml-auto mt-1" />
 
-            {/*Flex box with  upload button, video upload message, and number of cards to generate */}
             <div className="flex flex-col md:flex-row gap-0 w-4/5 mr-auto ml-auto mt-8">
                 <div className="w-full md:w-1/2 h-[12vh] flex items-center align-middle">
-                    <button onClick ={uploadContent} className="bg-teal-500 rounded-md p-4 text-white hover:bg-teal-600 active:scale-95">Upload {currentId}</button>
+                    <button onClick={uploadContent} className="bg-teal-500 rounded-md p-4 text-white hover:bg-teal-600 active:scale-95">Upload {currentId}</button>
                 </div>
-                {/* Number of Cards Input */}
                 <div className="w-full h-[12vh] md:w-1/2 flex flex-row gap-4 items-center align-middle">
-                    <h1 className = "text-gray-600 text-lg font-medium">Number of Cards:</h1>
-                    <input type="text" className="border rounded w-1/5 p-2" placeholder="15" />
+                    <h1 className="text-gray-600 text-lg font-medium">Number of Cards:</h1>
+                    <input type="text" className="border rounded w-1/5 p-2" value={numberOfCards} onChange={handleNumberOfCardsChange} placeholder="15" /> // Assigning value ensures the state is always in sync and controlled.
                 </div>
             </div>
 
-            {/*Text Box CONTAINER to enter schema */}
             <div className="w-4/5 ml-auto mr-auto mt-8">
                 <h1 className = "text-gray-600 text-lg font-medium">Describe what you want:</h1>
 
                 {/*Text Box to enter schema */}
-                <textarea type="text" className="border rounded w-full h-48 p-2 mt-4" placeholder="e.g Spanish words with their English translations based on the video">
+                <textarea type="text" className="border rounded w-full h-48 p-2 mt-4" value={currentSchema} onChange={changeCurrentSchema} placeholder="e.g Spanish words with their English translations based on the video">
                 </textarea>
 
                 {/*Submit Button */}
