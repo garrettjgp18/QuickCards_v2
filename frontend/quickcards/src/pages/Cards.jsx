37b0eda41f01443015ac0fa025f5e1276b48bcf3
Ali Kesserwani <akesser@uab.edu>
2024-04-03 18:32:52 -0500

    flashcards added

--- frontend/quickcards/src/pages/Cards.jsx
+++ frontend/quickcards/src/pages/Cards.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 export default function Words(){
 
     //dictionary loaded in from dexie
@@ -12,36 +11,65 @@ export default function Words(){
         "No": "no",
         "Si": "yes"
     };
-    
 
     //convert dictionary to an array to grab index
     const keyByIndex = Object.keys(dictionary);
 
     //create currentIndex reactive variable to change current increment or decrement current index
     const [currentIndex, changeIndex] = useState(0);
-    console.log(currentIndex);
 
-    //current card value - get value from dictionary
+    //current card value, stores word - get value from dictionary
     const [currentCard, changeCard] = useState(keyByIndex[currentIndex]);
-
-    console.log(currentCard);
 
     //go to next card
     const nextCard = () =>{
-        changeIndex(currentIndex + 1);
-        changeCard(keyByIndex[currentIndex]);
+
+        //define changing index variable
+        let nextIndex;
+
+        //If current index reaches end of the deck
+        if (currentIndex ==  keyByIndex.length - 1){
+            //set index to 0
+            nextIndex = 0;
+        } else{
+            //else increment card by 1 
+            nextIndex = currentIndex + 1;
+        }
+
+        // Update the currentIndex state
+        changeIndex(nextIndex);
+        //change card text by loading in current index
+        changeCard(keyByIndex[nextIndex]);
     }
 
     //go to previous card
     const prevCard = () =>{
-        if (currentIndex == 0){
-            changeIndex(currentIndex);
-        } else {
-            changeIndex(currentIndex - 1);
+        //define changing index variable
+        let prevIndex;
+
+        //If current index reaches end of the deck
+        if (currentIndex ==  0){
+            //set index to 0
+            prevIndex = keyByIndex.length - 1;
+        } else{
+            //else decrement card by 1 
+            prevIndex = currentIndex - 1;
         }
+
+        // Update the currentIndex state
+        changeIndex(prevIndex);
+        //change card text by loading in current index
+        changeCard(keyByIndex[prevIndex]);
+
     }
 
+    const [flipped, changeFlip] = useState(false);
 
+    //flip card
+    const flipCard = () =>{
+        //toggle true false variable
+        changeFlip(!flipped);
+    }
 
 
     return (
@@ -51,9 +79,12 @@ export default function Words(){
         <div className = "min-h-[70vh] w-3/4 m-auto mt-12">
 
             {/*Flashcard */}
-            <div className = "w-3/4 h-[50vh] border m-auto mt-4 rounded-xl shadow-lg overflow-auto hover:bg-gray-50 hover:cursor-pointer active:scale-95 flex items-center justify-center">
+            <div onClick = {flipCard} className = "w-3/4 h-[50vh] border m-auto mt-4 rounded-xl shadow-lg overflow-auto hover:bg-gray-50 hover:cursor-pointer active:scale-95 flex items-center justify-center">
                 {/*Flashcard Content*/}
-                <h1 className = "text-3xl text-gray-500">{ currentCard }</h1>
+
+               {/*If flipped variable is false, currentCard shows, if flipped is true, dictionary[currentCard] to show definition  */}
+                {flipped ? <h1 className="text-3xl text-gray-500 select-none">{currentCard}</h1> :<h1 className="text-3xl text-gray-500 select-none">{dictionary[currentCard] }</h1>}
+
             </div>
 
             {/*Buttons - Container*/}
@@ -65,7 +96,8 @@ export default function Words(){
 
                 {/*Display Card Number*/}
                 <div className="flex items-center">
-                    <h1 className = "text-gray-600 text-2xl">15/30</h1>
+                    {/* Current Index + 1 / Length of Deck */}
+                    <h1 className = "text-gray-600 text-2xl select-none"> { currentIndex + 1 } / { keyByIndex.length } </h1>
                 </div>
 
                 {/*Next Card - Right Button*/}
